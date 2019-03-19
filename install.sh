a3a87917d264608c0b06644806bcf083b6cf2d87
daniel <russelldan554@gmail.com>
2019-03-19 14:55:00 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-get -y install software-properties-common
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
